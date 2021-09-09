f2f1978ef0caed3a1f1ea867909bbf454a46742c
Talenta27 <90179830+Talenta27@users.noreply.github.com>
2021-09-09 12:17:23 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<div class="footer">
+			<div class="footer-middle">
+				<div class="container">
+					<div class="footer-middle-in">
+						<h6>Tentang Ulosta</h6>
+						<p>Ulosta adalah toko online yang bergerak di bidang kain ulos, sasaran kami untuk memenuhi kebutuhan acara adat istiadat</p>
+					</div>
+					<div class="footer-middle-in">
+					
+					</div>
+					<div class="footer-middle-in">
+						<h6 style="margin-left: 60px;">Alamat Kami</h6>
+						<p style="margin-left: 60px;">jln.Pelikan Raya no 01 Perumnas Mandala, Medan<br> Telp : 085276017761</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<p class="footer-class">Copyright © 2019 PA1 by  <a href="#">Kel'18</a> </p>
+			<script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear' 
+					 		};
+							*/
+							
+							$().UItoTop({ easingType: 'easeOutQuart' });
+							
+						});
+					</script>
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+		</div>
+		</body>
+		</html>
